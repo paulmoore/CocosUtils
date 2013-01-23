0901abab9af3c1ad673b6c376ca39d74370f50a5
Paul Moore <paul.andrewharrison.moore@gmail.com>
2013-01-22 19:20:55 -0800

    Initial Commit

---
+++ Cocos Utils/Cocos_Utils.h
@@ -0,0 +1,13 @@
+//
+//  Cocos_Utils.h
+//  Cocos Utils
+//
+//  Created by Paul Moore on 2013-01-22.
+//  Copyright (c) 2013 Paul Moore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cocos_Utils : NSObject
+
+@end
